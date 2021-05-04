16067c596df3d5aa2f9fc951a70673ca1a1e71db
Muhammad Daffa <36522826+daffainfo@users.noreply.github.com>
2021-05-04 17:23:36 +0700

    3d, kurang folder hidden

--- soal3/soal3.c
+++ soal3/soal3.c
@@ -7,7 +7,7 @@
 #include <ctype.h>
 
 //fungsi buat cek eksis file
-int cfileexists(const char* filename){
+int cfileexists(const char* filename) {
     struct stat buffer;
     int exist = stat(filename,&buffer);
     if(exist == 0)
@@ -18,17 +18,20 @@ int cfileexists(const char* filename){
 
 //fungsi move
 void *move(void *filename) {
-	char dirname[200];
-	char file[100];
-	char existsfile[100];
+	char dirname[200], file[100], existsfile[100], cok[100];
+    char *token1;
+    int i;
 	strcpy(existsfile, filename);
 
 	//dapat extensi
 	if (strstr(filename, ".") != NULL) {
 		strcpy(file, filename);
-		strtok(file, ".");
-		char * token = strtok(NULL, "");
-		strcpy(dirname, token);
+		token1 = strtok(file, ".");
+		strtok(NULL, "");
+		for(i = 0; token1[i]; i++){
+  			token1[i] = tolower(token1[i]);
+		}
+		strcpy(dirname, token1);
 	} else {
 		strcpy(dirname, "Unknown");
 	}
@@ -54,6 +57,7 @@ int main(int argc, char *argv[]) {
 	struct dirent *ep;
 	struct stat buffer;
 	char path[100];
+    int n = 0;
 
 	//Ketika ada argument -f
 	if (strcmp(argv[1], "-f") == 0) {
@@ -61,38 +65,35 @@ int main(int argc, char *argv[]) {
 		int i;
 		//buat thread dan print msg
 		for (i = 2; i < argc; i++) {
-			char printbjir[1000];
+			char printmsg[1000];
 			int exist = cfileexists(argv[i]);
 			if (exist) {
-				sprintf(printbjir,"File %d : Berhasil Dikategorikan", i-1);
-				printf("%s\n", printbjir);
+				sprintf(printmsg,"File %d : Berhasil Dikategorikan", i-1);
 			} else {
-				sprintf(printbjir,"File %d : Sad, gagal :(", i-1);
-				printf("%s\n", printbjir);
+				sprintf(printmsg,"File %d : Sad, gagal :(", i-1);
 			}
+            printf("%s\n", printmsg);
 			int err = pthread_create(&thread, NULL, move, (void *)argv[i]);
 		}
 		//join semua thread
 		pthread_join(thread, NULL);
-	}
-
-	else {
+	} else {
 		//ketika ada *
-		if (argv[1][0] == '*') {
+		if (strcmp(argv[1], "*") == 0) {
 			//membuka working directory sendiri
 			dp = opendir("/home/daffainfo/modul3/");
 			strcpy(path, "/home/daffainfo/modul3/");
 		}
 
 		//ketika ada argument -d
-		else if (strcmp(argv[1], "-d") == 0){
+		else if (strcmp(argv[1], "-d") == 0) {
 			//open direktori sesuai argument kedua
 			dp = opendir(argv[2]);
 			strcpy(path, argv[2]);
 		}
 
 		//buka direktori
-        if (dp != NULL){
+        if (dp != NULL) {
 			//check nanti berisi full path file
             char check[200];
             while(ep = readdir(dp)){
@@ -107,10 +108,14 @@ int main(int argc, char *argv[]) {
                     int err = pthread_create(&thread, NULL, move, (void*)check);
                     pthread_join(thread,NULL);
                 }
-
+				++n;
             }
+			//Semisal direktori gaada file didalamnya, maka print gagal
+			if (n <= 2)
+				printf("Yah, gagal disimpan :(\n");
             (void) closedir(dp);
         } else {
+			//Jika dir gaada print gagal
             printf("Yah, gagal disimpan :(\n");
         }
     }
